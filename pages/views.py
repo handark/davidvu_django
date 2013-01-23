069f26be6f9e17f2bcd8278b8bc9f81e28706914
David Velásquez <zero.blend@gmail.com>
2013-01-23 09:51:49 -0500

    Agregada la api de twitter con python-twitter

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,24 @@ from pages.models import Page
 
 def home(request):
     #homepage = get_object_or_404(Page, pk = 1)
-    return render_to_response('website/index.html')
+    return render_to_response('website/index.html', {'tweets': getTweets()})
 
 def view(request, page_slug):
     page = get_object_or_404(Page, slug = page_slug)
     return render_to_response('website/page_view.html', {'page': page})
+
+""" Funcion interna, no se llama por url """
+def getTweets():
+    tweets = []
+    try:
+        import twitter
+        api = twitter.Api()
+        latest = api.GetUserTimeline('zeroblend', count=2)
+        for tweet in latest:
+            status = tweet.text
+            tweet_date = tweet.relative_created_at
+            tweet_id = tweet.id
+            tweets.append({'status': status, 'date': tweet_date, 'id': tweet_id})
+    except:
+        tweets.append({'status': 'Sigueme en @zeroblend', 'date': ''})
+    return {'tweets': tweets}
