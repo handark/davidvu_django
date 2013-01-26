08ff91f92761104a83f531dd377b739fff66630f
David Velásquez <zero.blend@gmail.com>
2013-01-26 13:21:59 -0500

    Internationalization finalizada

--- pages/models.py
+++ pages/models.py
@@ -5,14 +5,16 @@ from tinymce import models as tinymce_models
 
 class Page(models.Model):
     title = models.CharField(max_length=100)
+    title_en = models.CharField(max_length=100)
     slug = models.SlugField(unique=True) 
     body = tinymce_models.HTMLField()
+    body_en = tinymce_models.HTMLField()
     
     def __unicode__(self):
         return self.title
     
 class ContactForm(forms.Form):
-    asunto = forms.CharField(max_length=100)
-    mensaje = forms.CharField(_('mensaje'))
-    correo = forms.EmailField(_('correo'))
+    asunto = forms.CharField(max_length=100, label=_('Asunto'))
+    mensaje = forms.CharField(label=_('Mensaje'))
+    correo = forms.EmailField(label=_('Correo'))
     #cc_myself = forms.BooleanField()
