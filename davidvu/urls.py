7f2aea01c2a9b84256249ca10e3221dcb8c6d997
David Velásquez <zero.blend@gmail.com>
2013-01-30 12:44:18 -0500

    robots.txt

--- davidvu/urls.py
+++ davidvu/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic.simple import direct_to_template
 #from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
     (r'^i18n/', include('django.conf.urls.i18n')),
+    (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
 )  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns('',
